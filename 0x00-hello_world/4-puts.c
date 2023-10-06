12858badb4100c445e1fbcdcdb021041322391ff
MrWavyEnt <chimacollinsekoma@gmail.com>
2023-10-05 17:37:48 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#Include <stdio.h>
+/**
+ * main - this is the prinf put function
+ * Return: 0
+ */
+int main(void)
+{
+	 print("with proper grammer, but the outcome is a piece of art\n")
+		 return (0)
+}
