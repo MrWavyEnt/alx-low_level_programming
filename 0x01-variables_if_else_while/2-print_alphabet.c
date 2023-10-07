db9e5681b65ccbd87a9bf0c21534d02dc573439f
MrWavyEnt <chimacollinsekoma@gmail.com>
2023-10-07 12:43:07 -0700

    insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function 
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+|
+for (1 = 'a'; 1 <= '2' ; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
