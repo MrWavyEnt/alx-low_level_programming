833685a8c4e37c7e02c366183cfcedad7d1134be
MrWavyEnt <chimacollinsekoma@gmail.com>
2023-10-08 12:34:02 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,20 +2,12 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char *message = "Programming is like building a multilingual puzzle";
-
-    /* Display the message using the puts function */
-    puts("[Got]");
-    puts(message);
-    printf("(%lu chars long)\n", strlen(message));
-
-    /* Return 0 to indicate success */
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
 
