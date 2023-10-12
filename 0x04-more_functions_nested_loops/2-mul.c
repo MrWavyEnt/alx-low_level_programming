2381ed6c05f6887b8b35ef4308e9d5567a565b0f
MrWavyEnt <chimacollinsekoma@gmail.com>
2023-10-12 06:17:59 -0700

    Collaboration

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies to intigers
+ * @a: function parameter 1
+ * @b: function parameter 2
+ * Return: returns void
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
