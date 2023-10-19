4ae9102762b6d523204f4726b7d03c8db0303701
MrWavyEnt <chimacollinsekoma@gmail.com>
2023-10-19 02:24:42 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - function name
+ * @dest: parameter one
+ * @src: parameter two
+ * @n: parameter tree
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+}
+return (dest);
+}
