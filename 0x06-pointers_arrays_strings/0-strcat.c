fdbe1b40da877381a0f82fd55cee942d51a2f529
MrWavyEnt <chimacollinsekoma@gmail.com>
2023-10-20 14:53:32 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i;
-int j;
+int i, j;
 
 i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
-j = 0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
 
-dest[i] = '\0';
+/*find the size of dest array*/
+while (dest[i])
+i++;
+
+/* interate through each src array value without the null byte*/
+for (j = 0; src[j] ; j++)
+/*append src[c2] to dest[c] while overwritting the nullbyte in dest*/
+dest[i++] = src[j];
+
 return (dest);
 }
