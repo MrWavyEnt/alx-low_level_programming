ffdf7109a4232c54946c3fc8a682cd2f214032d7
MrWavyEnt <chimacollinsekoma@gmail.com>
2023-11-09 03:57:47 -0800

    3-main.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,15 +18,15 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	
+
 	int i = 0;
 
 	while (ops[i].op != NULL)
 	{
 		if (strcmp(s, ops[i].op) == 0)
-			return ops[i].f;
+			return (ops[i].f);
 		i++;
 	}
 
-	return NULL;
+	return (NULL);
 }
