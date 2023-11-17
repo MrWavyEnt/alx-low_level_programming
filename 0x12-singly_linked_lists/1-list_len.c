f27472a42d9199597a43a080e9e09a151f8a28c2
MrWavyEnt <chimacollinsekoma@gmail.com>
2023-11-17 13:17:10 -0800

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,18 +4,17 @@
 /**
  * list_len - returns the number of elements in a linked list
  * @h: pointer to the list_t list
- *
  * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
-	size_t count = 0;
+	int i;
 
-	while (h)
-	{
+	if (h == NULL)
+		return (0);
+
+	for (i = 1; h->next != NULL; i++)
 		h = h->next;
-		count++;
-	}
 
-	return (count);
+	return (i);
 }
