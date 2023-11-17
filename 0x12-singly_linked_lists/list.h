da590d76266e5ec876a2fac6131c18096c712872
MrWavyEnt <chimacollinsekoma@gmail.com>
2023-11-16 23:14:01 -0800

    list.h

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,16 +1,17 @@
 #ifndef LINKED
 #define LINKED
 
-#include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string
+ * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- * Description: singly linked llist
+ *
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
@@ -19,7 +20,6 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
