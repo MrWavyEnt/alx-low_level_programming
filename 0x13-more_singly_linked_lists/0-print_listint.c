dd721f6d863432a2f30f97cba27bd2b282aeaf95
MrWavyEnt <chimacollinsekoma@gmail.com>
2023-11-21 12:28:56 +0200

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,7 +13,7 @@ size_t print_listint(const listint_t *h)
 		return (0);
 	for (count = 0; h != NULL; count++)
 	{
-		printf("%d\n", h ->n);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (count);
