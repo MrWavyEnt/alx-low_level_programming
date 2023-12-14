0e24a780d64a6a236aee8d98a6a9c70f092e879d
MrWavyEnt <chimacollinsekoma@gmail.com>
2023-12-14 12:36:37 +0200

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,8 +23,9 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	for
-		(newletters = 0; text_content[newletters]; newletters++)
-			;
+		(newletters = 0;
+		text_content[newletters];
+		newletters++);
 
 	x = write(y, text_content, newletters);
 
