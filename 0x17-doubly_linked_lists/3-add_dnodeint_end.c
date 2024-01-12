cae362ea36b84dd7ec4331addab4f3f958c57a47
MrWavyEnt <chimacollinsekoma@gmail.com>
2024-01-12 23:57:26 +0200

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -25,11 +25,13 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		while (h->next != NULL)
 			h = h->next;
+		h->next = new;
+		new->prev = h;
 	}
 	else
 	{
 		*head = new;
+		new->prev = NULL;
 	}
-	new->prev = h;
 		return (new);
 }
